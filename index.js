b6adac54ba293285927068d891473fe3d1417d3b
PrabhatAcharya <code2prabhat@gmail.com>
2023-05-06 23:44:20 +0530

    updateFrontendLink

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 const PORT = process.env.PORT || 9000;
 const io = new Server(PORT, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://prabhat-whatsapp-clone.netlify.app/",
   },
 });
 
